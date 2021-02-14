275edd368a644f084b2035060c87f620e2be924d
poramme wittayanupong <76250724+piporamme@users.noreply.github.com>
2021-02-14 22:36:43 +0700

    Add files via upload

---
+++ LAB3_1/basic1.js
@@ -0,0 +1,9 @@
+var n=prompt("What is your favorite charactor?");
+var c=prompt("What is the main color of "+ n +"?" );
+console.log("%s is your favorite charactor",n);
+console.log(n + " is " + c);
+
+
+
+
+
