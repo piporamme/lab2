99ed6d0e5af9e394a5e8d95afe8d013e2ae7e7ef
poramme wittayanupong <76250724+piporamme@users.noreply.github.com>
2021-02-20 22:48:50 +0700

    Update script.js

--- LAB4_1/script.js
+++ LAB4_1/script.js
@@ -1,1 +1,23 @@
-const 
+const b=document.querySelectorAll(".b");
+const w=document.querySelectorAll(".w");
+console.log(w.length)
+
+for(let i=0;i<w.length;i++){
+    w[i].addEventListener('click',swabza);
+}
+for(let i=0;i<b.length;i++){
+    b[i].addEventListener('click',swabza);
+}
+
+function swabza(){
+    for(let i=0;i<w.length;i++){
+        if(w[i].className === "b"){
+            w[i].className = "w";
+            b[i].className = "b";
+        }
+        else if(w[i].className === "w"){
+            w[i].className = "b";
+            b[i].className = "w";
+        }
+    }
+}
